ed7dd6ef497fae38e64cd552464f96c6c2d8c6c2
Josue Rivas <josu.riva@gmail.com>
2022-04-17 09:50:21 -0600

    primer commit

---
+++ app/src/test/java/dsm/udb/rg180141/gg162362/mr171225/rp142494/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package dsm.udb.rg180141.gg162362.mr171225.rp142494;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
